31b9a5e3df672dc99ef0bd454ff1d34486c995c6
renanzdm <renanzdm@gmail.com>
2023-02-27 07:55:06 -0300

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import '../style/index.css'
+
+function App() {
+  return (
+    <div className='center'>
+      <img className='avatarImage' src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="profile" />
+    </div>
+  )
+}
+
+export default App
